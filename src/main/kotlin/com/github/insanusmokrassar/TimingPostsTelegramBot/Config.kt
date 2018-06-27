baa81380d0ace7e5cc0add702c104a8ae7d80001
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-06-27 15:16:49 +0800

    add checking target chats

--- src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Config.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Config.kt
@@ -1,11 +1,19 @@
 package com.github.insanusmokrassar.TimingPostsTelegramBot
 
+import org.h2.Driver
+
 class Config (
     val targetChatId: String? = null,
     val sourceChatId: String? = null,
     val adminChatId: String? = null,
     val botToken: String? = null,
-    val databaseConfig: DatabaseConfig? = null
+    val databaseConfig: DatabaseConfig = DatabaseConfig(
+        "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+        Driver::class.java.canonicalName,
+        "sa",
+        ""
+    ),
+    val debug: Boolean = false
 ) {
     val finalConfig: FinalConfig
         @Throws(IllegalArgumentException::class)
@@ -13,8 +21,9 @@ class Config (
             targetChatId ?: throw IllegalArgumentException("Target chat id (field \"targetChatId\") can't be null"),
             sourceChatId ?: throw IllegalArgumentException("Source chat id (field \"sourceChatId\") can't be null"),
             botToken ?: throw IllegalArgumentException("Bot token (field \"botToken\") can't be null"),
-            databaseConfig ?: throw IllegalArgumentException("Database config (field \"databaseConfig\") can't be null"),
-            adminChatId
+            databaseConfig,
+            adminChatId,
+            debug
         )
 }
 
@@ -30,5 +39,6 @@ class FinalConfig (
     val sourceChatId: String,
     val botToken: String,
     val databaseConfig: DatabaseConfig,
-    val adminChatId: String? = null
+    val adminChatId: String? = null,
+    val debug: Boolean = false
 )
--- src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Launch.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Launch.kt
@@ -1,9 +1,38 @@
 package com.github.insanusmokrassar.TimingPostsTelegramBot
 
+import com.github.insanusmokrassar.BotIncomeMessagesListener.BotIncomeMessagesListener
+import com.github.insanusmokrassar.BotIncomeMessagesListener.UpdateCallback
+import com.github.insanusmokrassar.IObjectK.interfaces.IObject
 import com.github.insanusmokrassar.IObjectKRealisations.*
+import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.model.Chat
+import com.pengrad.telegrambot.model.Message
+import com.pengrad.telegrambot.request.GetChat
 
 fun main(args: Array<String>) {
     val config = load(args[0]).toObject(Config::class.java).finalConfig
 
-    println(config.toJson())
+    val bot = TelegramBot.Builder(
+        config.botToken
+    ).run {
+        if (config.debug) {
+            debug()
+        }
+        build()
+    }
+    var willHaveMessages = false
+
+    bot.execute(GetChat(config.sourceChatId)).let {
+        willHaveMessages = it.chat() ?.type() ?.let {
+            it != Chat.Type.channel
+        } ?: false
+    }
+
+    bot.execute(GetChat(config.targetChatId)).let {
+        willHaveMessages = it.chat() ?.type() ?.let {
+            it != Chat.Type.channel
+        } ?: false
+    }
+
+
 }
