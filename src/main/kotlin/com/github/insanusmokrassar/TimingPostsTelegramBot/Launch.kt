d987b7528d8556406a67ba7ea65f24d4142ddd00
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-06-30 17:41:29 +0800

    add VoiceForwarder

--- src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Launch.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/Launch.kt
@@ -69,6 +69,7 @@ fun main(args: Array<String>) {
             VideoForwarder(),
             MediaGroupForwarder(),
             AudioForwarder(),
+            VoiceForwarder(),
             DocumentForwarder(),
             TextForwarder(),
             SimpleForwarder()
