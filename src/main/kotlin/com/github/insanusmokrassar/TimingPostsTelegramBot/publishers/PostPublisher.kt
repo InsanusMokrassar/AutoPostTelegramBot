7ae9b0a7e10f64d489d7f0b6003ee668bb74b003
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-07-03 11:01:11 +0800

    fix post publisher

--- src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/publishers/PostPublisher.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/publishers/PostPublisher.kt
@@ -3,12 +3,12 @@ package com.github.insanusmokrassar.TimingPostsTelegramBot.publishers
 import com.github.insanusmokrassar.TimingPostsTelegramBot.commands.deletePost
 import com.github.insanusmokrassar.TimingPostsTelegramBot.database.tables.PostsLikesTable
 import com.github.insanusmokrassar.TimingPostsTelegramBot.database.tables.PostsMessagesTable
+import com.github.insanusmokrassar.TimingPostsTelegramBot.extensions.executeAsync
 import com.github.insanusmokrassar.TimingPostsTelegramBot.forwarders.Forwarder
 import com.github.insanusmokrassar.TimingPostsTelegramBot.models.PostMessage
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.model.request.ParseMode
-import com.pengrad.telegrambot.request.ForwardMessage
-import com.pengrad.telegrambot.request.SendMessage
+import com.pengrad.telegrambot.request.*
 import java.lang.ref.WeakReference
 
 class PostPublisher(
@@ -24,89 +24,100 @@ class PostPublisher(
 
         val messagesToDelete = mutableListOf<Int>()
 
-        bot.execute(
-            SendMessage(
-                sourceChatId,
-                "Start post"
-            )
-        ) ?. message() ?. messageId() ?.let {
-            messagesToDelete.add(it)
-        }
-
-        val messageToPost = PostsMessagesTable.getMessagesOfPost(postId).also {
-            it.forEach { message ->
-                messagesToDelete.add(message.messageId)
-
-                bot.execute(
-                    ForwardMessage(
-                        sourceChatId,
-                        sourceChatId,
-                        message.messageId
-                    ).disableNotification(
-                        true
-                    )
-                ).also {
-                    messagesToDelete.add(it.message().messageId())
-                    message.message = it.message()
-                }
-            }
-        }
-
-        val mapOfExecution = mutableListOf<Pair<Forwarder, MutableList<PostMessage>>>()
-
-        var forwarder: Forwarder? = null
-
-        messageToPost.forEach { message ->
-            if (forwarder?.canForward(message) != true) {
-                val iterator = forwardersList.iterator()
-                while (forwarder?.canForward(message) != true) {
-                    if (!iterator.hasNext()) {
-                        return@forEach
-                    }
-                    forwarder = iterator.next()
-                }
-            }
-            if (mapOfExecution.lastOrNull()?.first != forwarder) {
-                forwarder?.let {
-                    mapOfExecution.add(
-                        it to mutableListOf()
-                    )
-                }
-            }
-            mapOfExecution.last().second.add(message)
-        }
-
-        mapOfExecution.flatMap {
-            it.first.forward(
-                bot,
-                targetChatId,
-                *it.second.toTypedArray()
-            )
-        }.let {
+        try {
             bot.execute(
                 SendMessage(
                     sourceChatId,
-                    "Post published. Rating: ${PostsLikesTable.getPostRating(postId)}"
-                ).parseMode(
-                    ParseMode.Markdown
+                    "Start post"
                 )
-            )
-            it.forEach {
+            )?.message()?.messageId()?.let {
+                messagesToDelete.add(it)
+            }
+
+            val messageToPost = PostsMessagesTable.getMessagesOfPost(postId).also {
+                it.forEach { message ->
+                    messagesToDelete.add(message.messageId)
+
+                    bot.execute(
+                        ForwardMessage(
+                            sourceChatId,
+                            sourceChatId,
+                            message.messageId
+                        ).disableNotification(
+                            true
+                        )
+                    ).also {
+                        messagesToDelete.add(it.message().messageId())
+                        message.message = it.message()
+                    }
+                }
+            }
+
+            val mapOfExecution = mutableListOf<Pair<Forwarder, MutableList<PostMessage>>>()
+
+            var forwarder: Forwarder? = null
+
+            messageToPost.forEach { message ->
+                if (forwarder?.canForward(message) != true) {
+                    val iterator = forwardersList.iterator()
+                    while (forwarder?.canForward(message) != true) {
+                        if (!iterator.hasNext()) {
+                            return@forEach
+                        }
+                        forwarder = iterator.next()
+                    }
+                }
+                if (mapOfExecution.lastOrNull()?.first != forwarder) {
+                    forwarder?.let {
+                        mapOfExecution.add(
+                            it to mutableListOf()
+                        )
+                    }
+                }
+                mapOfExecution.last().second.add(message)
+            }
+
+            mapOfExecution.flatMap {
+                it.first.forward(
+                    bot,
+                    targetChatId,
+                    *it.second.toTypedArray()
+                )
+            }.let {
                 bot.execute(
-                    ForwardMessage(
+                    SendMessage(
                         sourceChatId,
-                        targetChatId,
+                        "Post published. Rating: ${PostsLikesTable.getPostRating(postId)}"
+                    ).parseMode(
+                        ParseMode.Markdown
+                    )
+                )
+                it.forEach {
+                    bot.execute(
+                        ForwardMessage(
+                            sourceChatId,
+                            targetChatId,
+                            it
+                        )
+                    )
+                }
+            }
+
+            deletePost(
+                bot,
+                sourceChatId,
+                postId
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            messagesToDelete.forEach {
+                bot.executeAsync(
+                    DeleteMessage(
+                        sourceChatId,
                         it
                     )
                 )
             }
         }
-
-        deletePost(
-            bot,
-            sourceChatId,
-            postId,
-            *messagesToDelete.toIntArray()
-        )
     }
 }
