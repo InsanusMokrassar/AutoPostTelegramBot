ecccea9e118e6f8628fa2b3e145066098756414b
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-07-06 20:56:34 +0800

    add handling of case when post have no messages

--- src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/publishers/PostPublisher.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TimingPostsTelegramBot/publishers/PostPublisher.kt
@@ -1,8 +1,8 @@
 package com.github.insanusmokrassar.TimingPostsTelegramBot.publishers
 
 import com.github.insanusmokrassar.TimingPostsTelegramBot.commands.deletePost
-import com.github.insanusmokrassar.TimingPostsTelegramBot.database.tables.PostsLikesTable
-import com.github.insanusmokrassar.TimingPostsTelegramBot.database.tables.PostsMessagesTable
+import com.github.insanusmokrassar.TimingPostsTelegramBot.database.exceptions.NoRowFoundException
+import com.github.insanusmokrassar.TimingPostsTelegramBot.database.tables.*
 import com.github.insanusmokrassar.TimingPostsTelegramBot.extensions.executeAsync
 import com.github.insanusmokrassar.TimingPostsTelegramBot.forwarders.Forwarder
 import com.github.insanusmokrassar.TimingPostsTelegramBot.models.PostMessage
@@ -35,23 +35,28 @@ class PostPublisher(
                 messagesToDelete.add(it)
             }
 
-            val messageToPost = PostsMessagesTable.getMessagesOfPost(postId).also {
-                it.forEach { message ->
-                    messagesToDelete.add(message.messageId)
+            val messageToPost = try {
+                PostsMessagesTable.getMessagesOfPost(postId).also {
+                    it.forEach { message ->
+                        messagesToDelete.add(message.messageId)
 
-                    bot.execute(
-                        ForwardMessage(
-                            sourceChatId,
-                            sourceChatId,
-                            message.messageId
-                        ).disableNotification(
-                            true
-                        )
-                    ) ?.message() ?.also {
-                        messagesToDelete.add(it.messageId())
-                        message.message = it
+                        bot.execute(
+                            ForwardMessage(
+                                sourceChatId,
+                                sourceChatId,
+                                message.messageId
+                            ).disableNotification(
+                                true
+                            )
+                        ) ?.message() ?.also {
+                            messagesToDelete.add(it.messageId())
+                            message.message = it
+                        }
                     }
                 }
+            } catch (e: NoRowFoundException) {
+                PostsTable.removePost(postId)
+                return
             }
 
             val mapOfExecution = mutableListOf<Pair<Forwarder, MutableList<PostMessage>>>()
