51c98a575a6b1afbff66ca9d853a183ebb186300
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-09-26 11:11:18 +0800

    rewrite SemaphoreK to use actor

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostTelegramBot/utils/SemaphoreK.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostTelegramBot/utils/SemaphoreK.kt
@@ -1,94 +1,81 @@
 package com.github.insanusmokrassar.AutoPostTelegramBot.utils
 
+import kotlinx.coroutines.experimental.channels.SendChannel
+import kotlinx.coroutines.experimental.channels.actor
 import kotlinx.coroutines.experimental.isActive
 import kotlinx.coroutines.experimental.launch
-import java.lang.IllegalArgumentException
 import java.util.*
-import java.util.concurrent.ConcurrentLinkedQueue
 import kotlin.coroutines.experimental.Continuation
 import kotlin.coroutines.experimental.suspendCoroutine
 
 class SemaphoreK(
     private val maxAvailable: Int = 1
 ) {
-    private val sync = Object()
-    var available: Int = maxAvailable
-        private set(value) {
-            synchronized(sync) {
-                field = value
-                if (field > maxAvailable) {
-                    field = maxAvailable
-                }
-            }
-        }
-        get() {
-            return synchronized(sync) {
-                field
-            }
-        }
-
-    private val suspenders = Array<Queue<Continuation<Unit>>>(maxAvailable) { ConcurrentLinkedQueue<Continuation<Unit>>() }
-    private var workerContinuation: Continuation<Unit>? = null
-
-    init {
-        if (maxAvailable < 1) {
-            throw IllegalArgumentException("Max available can't be less than 1, but was $maxAvailable")
-        }
-        launch {
-            while (isActive) {
-                synchronized(sync) {
-                    var currentLockCount = available
-                    while (available > 0 && currentLockCount > 0) {
-                        val queue = suspenders[currentLockCount - 1]
-                        if (queue.isNotEmpty()) {
-
-                            var current: Continuation<Unit>
-
-                            do {
-                                current = queue.poll()
-                            } while (!current.context.isActive)
-
-                            current.resume(Unit)
-                            available -= currentLockCount
-
-                            currentLockCount = available
-                        }
-                        currentLockCount--
-                    }
-                }
-
-                suspendCoroutine<Unit> {
-                    synchronized(sync) {
-                        workerContinuation = it
-                    }
-                }
-            }
-        }
+    private sealed class SemaphoreAction {
+        class IncAction(val count: Int = 1) : SemaphoreAction()
+        class BlockAction(val count: Int = 1, val continuation: Continuation<Unit>) : SemaphoreAction()
     }
 
-    private fun notifyWorker() {
-        synchronized(sync) {
-            workerContinuation ?.resume(Unit)
-            workerContinuation = null
+    private val semaphoreActor: SendChannel<SemaphoreAction>
+
+    init {
+        semaphoreActor = actor {
+            var free = maxAvailable
+
+            val continuations = Array<Queue<Continuation<Unit>>>(maxAvailable) { ArrayDeque() }
+
+            fun checkContinuations() {
+                for (i in (free - 1) downTo 0) {
+                    continuations[i].poll() ?.let {
+                        if (it.context.isActive) {
+                            it.resume(Unit)
+                            free -= (i + 1)
+                            checkContinuations()
+                        } else {
+                            null
+                        }
+                    } ?: continue
+                    break
+                }
+            }
+
+            for (msg in channel) {
+                when (msg) {
+                    is SemaphoreAction.IncAction -> {
+                        free += msg.count
+                        if (free > maxAvailable) {
+                            free = maxAvailable
+                        }
+                        checkContinuations()
+                    }
+                    is SemaphoreAction.BlockAction -> {
+                        continuations[msg.count - 1].offer(msg.continuation)
+                        checkContinuations()
+                    }
+                }
+            }
         }
     }
 
     suspend fun lock(count: Int = 1) {
-
         if (count < 1) {
             throw IllegalArgumentException("Lock count can't be less than 1, but was $count")
         }
 
         suspendCoroutine<Unit> {
-            suspenders[count - 1].offer(it)
-            notifyWorker()
+            launch {
+                semaphoreActor.send(
+                    SemaphoreAction.BlockAction(count, it)
+                )
+            }
         }
     }
 
     fun free(count: Int = 1) {
-        synchronized(sync) {
-            available += count
-            notifyWorker()
+        launch {
+            semaphoreActor.send(
+                SemaphoreAction.IncAction(count)
+            )
         }
     }
 }
