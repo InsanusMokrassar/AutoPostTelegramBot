6269fbee5064f028dc1e45529468b49efbdc1094
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-01-21 08:34:14 +0800

    fixes in SafeCallbackQueryReceiver

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostTelegramBot/utils/CallbackQueryReceivers/SafeCallbackQueryReceiver.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostTelegramBot/utils/CallbackQueryReceivers/SafeCallbackQueryReceiver.kt
@@ -1,14 +1,9 @@
 package com.github.insanusmokrassar.AutoPostTelegramBot.utils.CallbackQueryReceivers
 
-import com.github.insanusmokrassar.AutoPostTelegramBot.callbackQueryListener
-import com.github.insanusmokrassar.AutoPostTelegramBot.utils.NewDefaultCoroutineScope
-import com.github.insanusmokrassar.AutoPostTelegramBot.utils.extensions.subscribe
 import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.types.CallbackQuery.*
 import com.github.insanusmokrassar.TelegramBotAPI.types.ChatIdentifier
 import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.Update
-
-private val SafeCallbackQueryReceiversScope = NewDefaultCoroutineScope()
 
 abstract class SafeCallbackQueryReceiver(
     executor: RequestsExecutor,
@@ -16,13 +11,6 @@ abstract class SafeCallbackQueryReceiver(
 ) : CallbackQueryReceiver(
     executor
 ) {
-    init {
-        callbackQueryListener.subscribe(
-            scope = SafeCallbackQueryReceiversScope
-        ) { update ->
-        }
-    }
-
     override suspend fun invoke(update: Update<CallbackQuery>) {
         val query = update.data as? MessageDataCallbackQuery ?: return
         if (query.message.chat.id == checkChatId) {
