a00adc4cc7326997e3cde18113b84d898e2ea558
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-03-23 15:12:57 +0600

    rewrite mechanism of media groups for test purpose

--- src/main/kotlin/dev/inmo/AutoPostTelegramBot/Launch.kt
+++ src/main/kotlin/dev/inmo/AutoPostTelegramBot/Launch.kt
@@ -9,11 +9,12 @@ import dev.inmo.AutoPostTelegramBot.base.plugins.commonLogger
 import dev.inmo.AutoPostTelegramBot.utils.flow.collectWithErrors
 import dev.inmo.AutoPostTelegramBot.utils.flow.combineFlows
 import dev.inmo.AutoPostTelegramBot.utils.load
-import dev.inmo.micro_utils.coroutines.plus
-import dev.inmo.micro_utils.coroutines.safely
+import dev.inmo.micro_utils.coroutines.*
+import dev.inmo.tgbotapi.extensions.utils.shortcuts.mediaGroupId
 import dev.inmo.tgbotapi.extensions.utils.updates.filterBaseMessageUpdatesByChatId
 import dev.inmo.tgbotapi.requests.chat.get.GetChat
 import dev.inmo.tgbotapi.types.CallbackQuery.MessageDataCallbackQuery
+import dev.inmo.tgbotapi.types.MediaGroupIdentifier
 import dev.inmo.tgbotapi.types.mediaCountInMediaGroup
 import dev.inmo.tgbotapi.types.message.abstracts.MediaGroupMessage
 import dev.inmo.tgbotapi.types.update.CallbackQueryUpdate
@@ -64,56 +65,73 @@ fun main(args: Array<String>) {
     val mediaGroupsChannel = Channel<Pair<SentMediaGroupUpdate, List<BaseMessageUpdate>>>()
 
     scope.launch {
-        val currentList = mutableListOf<SentMediaGroupUpdate>()
+//        val currentList = mutableListOf<SentMediaGroupUpdate>()
         val mutex = Mutex()
-        suspend fun send() {
-            mediaGroupsChannel.send(currentList.first() to currentList.flatMap { it.origins })
-            currentList.clear()
-        }
-        var debounceJob: Job? = null
-        launch {
-            combineFlows(flowFilter.messageMediaGroupFlow, flowFilter.channelPostMediaGroupFlow, scope = scope).filter {
-                val mediaGroupChatId = it.data.first().chat.id
-                mediaGroupChatId == config.sourceChatId
-            }.collectWithErrors({ _, _ -> if (mutex.isLocked) mutex.unlock() }) {
-                debounceJob ?.cancelAndJoin()
-                mutex.withLock {
-                    val currentMediaGroup = it.data.first().mediaGroupId
-                    val previousMediaGroup = currentList.firstOrNull() ?.data ?.first() ?.mediaGroupId
-                    if (currentMediaGroup != previousMediaGroup && currentList.isNotEmpty()) {
-                        commonLogger.info("Current mediagroup do not equals to previous ($currentMediaGroup, $previousMediaGroup)")
-                        send()
-                    }
-                    currentList.add(it)
-                    if (currentList.size == mediaCountInMediaGroup.last) {
-                        send()
-                    } else {
-                        debounceJob = scope.launch {
-                            safely({ if (mutex.isLocked) mutex.unlock() }) {
-                                delay(5000L)
-                                mutex.withLock {
-                                    commonLogger.info("Sending after debounce")
-                                    if (currentList.isNotEmpty()) {
-                                        send()
-                                    }
-                                }
-                            }
-                        }
+//        suspend fun send() {
+//            mediaGroupsChannel.send(currentList.first() to currentList.flatMap { it.origins })
+//            currentList.clear()
+//        }
+//        var debounceJob: Job? = null
+
+        val mediaGroupsJobs = mutableMapOf<MediaGroupIdentifier, Pair<SentMediaGroupUpdate, Job>>()
+        (flowFilter.messageMediaGroupFlow + flowFilter.channelPostMediaGroupFlow).subscribeSafelyWithoutExceptions(scope) {
+            mutex.withLock {
+                mediaGroupsJobs[it.mediaGroupId] ?.second ?.cancel()
+                mediaGroupsJobs[it.mediaGroupId] = it to launch {
+                    delay(5000L)
+
+                    mutex.withLock {
+                        mediaGroupsChannel.send(
+                            it to it.origins
+                        )
+                        mediaGroupsJobs.remove(it.mediaGroupId)
                     }
                 }
             }
         }
-        launch {
-            checkedMessagesFlow.collectWithErrors {
-                debounceJob ?.cancelAndJoin()
-                mutex.withLock {
-                    if (currentList.isNotEmpty()) {
-                        commonLogger.info("Sending on non-mediagroup message")
-                        send()
-                    }
-                }
-            }
-        }
+//        launch {
+//            combineFlows(flowFilter.messageMediaGroupFlow, flowFilter.channelPostMediaGroupFlow, scope = scope).filter {
+//                val mediaGroupChatId = it.data.first().chat.id
+//                mediaGroupChatId == config.sourceChatId
+//            }.collectWithErrors({ _, _ -> if (mutex.isLocked) mutex.unlock() }) {
+//                debounceJob ?.cancelAndJoin()
+//                mutex.withLock {
+//                    val currentMediaGroup = it.data.first().mediaGroupId
+//                    val previousMediaGroup = currentList.firstOrNull() ?.data ?.first() ?.mediaGroupId
+//                    if (currentMediaGroup != previousMediaGroup && currentList.isNotEmpty()) {
+//                        commonLogger.info("Current mediagroup do not equals to previous ($currentMediaGroup, $previousMediaGroup)")
+//                        send()
+//                    }
+//                    currentList.add(it)
+//                    if (currentList.size == mediaCountInMediaGroup.last) {
+//                        send()
+//                    } else {
+//                        debounceJob = scope.launch {
+//                            safely({ if (mutex.isLocked) mutex.unlock() }) {
+//                                delay(5000L)
+//                                mutex.withLock {
+//                                    commonLogger.info("Sending after debounce")
+//                                    if (currentList.isNotEmpty()) {
+//                                        send()
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        launch {
+//            checkedMessagesFlow.collectWithErrors {
+//                debounceJob ?.cancelAndJoin()
+//                mutex.withLock {
+//                    if (currentList.isNotEmpty()) {
+//                        commonLogger.info("Sending on non-mediagroup message")
+//                        send()
+//                    }
+//                }
+//            }
+//        }
     }
     checkedMediaGroupsFlow = mediaGroupsChannel.consumeAsFlow().mapNotNull { (update, baseUpdates) ->
         when (update) {
